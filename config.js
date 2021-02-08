eda7f7d606cec4f57bc9b1dc59da099e0cea9b00
Noura Abdelhafez <44889573+noura-na@users.noreply.github.com>
2021-02-08 10:04:58 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
-  description: 'Full-stack Web Developer',
-  primaryColor: '#3498db', // Required
+  title: 'Blog', // Required
+  author: 'Noura Abdelhafez', // Required
+  description: 'Data Scince and Artificial Intelligence Student',
+  primaryColor: '#EFF377', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    website: 'noura-na.github.io/portfolio',
+    github: 'https://github.com/noura-na',
+    //twitter: 'https://twitter.com/ryanafitzgerald',
+    linkedin: 'https://www.linkedin.com/in/noura-abdelhafez/',
   },
   pathPrefix: '/devblog',
   siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
